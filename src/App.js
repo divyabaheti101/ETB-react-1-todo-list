e23812c04ac0ef23e5e16d4ed5c6f2f762414072
Divya <divyabaheti101@gmail.com>
2023-12-06 11:28:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodos(text) {
+    setTodos([...todos, text]);
+  }
+
+  function removeTodos(index) {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
+  function editTodo(index, text) {
+    const newTodos = [...todos];
+    newTodos[index] = text;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        addTodos(event.target.elements.todo.value);
+        event.target.elements.todo.value = "";
+      }}>
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input type="text" value={todo} onChange={(event) => editTodo(index, event.target.value)} />
+            <button onClick={() => {removeTodos(index)}}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
